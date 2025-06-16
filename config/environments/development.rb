9bfbfd76dda86c7eb7c86382615b5925deb881e7
Mohammad AL-MOUSAWI <malmousa@guell.cs.ucl.ac.uk>
2025-06-16 11:01:38 +0100

    changed development.rb file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 require "active_support/core_ext/integer/time"
-
+config.hosts << "128.16.12.219"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
