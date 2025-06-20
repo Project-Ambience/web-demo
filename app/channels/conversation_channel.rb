2530bfefd597b99680aa6f8e5017768abcbe492d
Saruj Sattayanurak <saruj@Sarujs-MacBook-Pro.local>
2025-06-20 12:51:24 +0100

    Delete unrelated cable code

--- app/channels/conversation_channel.rb
+++
@@ -1,5 +0,0 @@
-class ConversationChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "conversation_#{params[:conversation_id]}"
-  end
-end
--- app/helpers/api/internal/results_helper.rb
+++
@@ -1,2 +0,0 @@
-module Api::Internal::ResultsHelper
-end
