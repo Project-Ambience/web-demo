8fb46077284cb2788b80e3da42df918dcfc39cb4
Saruj.S <saruj.sattayanurak@gmail.com>
2025-07-04 13:04:24 +0100

    PA56: Separate RabbitMQ out as an individual container (#20)
    Co-authored-by: Saruj Sattayanurak <saruj@Sarujs-MacBook-Pro.local>

--- app/services/message_publisher.rb
+++ app/services/message_publisher.rb
@@ -2,7 +2,13 @@ require "bunny"
 
 class MessagePublisher
   def self.publish(payload, queue)
-    connection = Bunny.new(hostname: "rabbitmq")
+    connection = Bunny.new(
+      hostname: ENV["DOMAIN"],
+      port: ENV["RABBITMQ_PORT"],
+      user: ENV["RABBITMQ_USER"],
+      password: ENV["RABBITMQ_PASSWORD"],
+      vhost: "/"
+    )
     connection.start
     channel = connection.create_channel
     queue = channel.queue(queue, durable: true)
