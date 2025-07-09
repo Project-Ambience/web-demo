28b0d8077115c6abfbe6b86606fdbc829b7deeed
Saruj Sattayanurak <saruj@Sarujs-MacBook-Pro.local>
2025-07-09 15:32:44 +0100

    Modify UI

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -253,10 +253,11 @@ const Message = styled.div`
   }
 
   &[data-role="assistant"] {
-    background-color: #fff;
+    background-color: #eaf1f8;
     color: #1f1f1f;
     margin-right: auto;
-    border-bottom-left-radius: 5px;
+    border-top-left-radius: 5px;
+    border-bottom-right-radius: 20px;
   }
 `;
 
