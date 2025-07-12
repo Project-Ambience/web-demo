bca271cb75f0d431ac7d4b77a44cec464f38fee4
William LI <junzhanl@guell.cs.ucl.ac.uk>
2025-07-12 12:53:42 +0100

    Fixed database model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,7 +2,7 @@ class Conversation < ApplicationRecord
   belongs_to :ai_model
   has_many :messages, -> { order(created_at: :asc) }, dependent: :destroy
 
-  enum status: {
+  enum :status, {
     awaiting_prompt: 0,
     awaiting_feedback: 1,
     awaiting_rejection_comment: 2,
