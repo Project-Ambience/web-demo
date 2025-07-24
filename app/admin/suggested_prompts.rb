54f30d2cbb25f2cb84c01cb16461091bed551a8f
Saruj Sattayanurak <saruj@Sarujs-MacBook-Pro.local>
2025-07-24 16:39:03 +0100

    Change in admin

--- app/admin/suggested_prompts.rb
+++ app/admin/suggested_prompts.rb
@@ -2,4 +2,14 @@ ActiveAdmin.register SuggestedPrompt do
   permit_params :prompt, :ai_model_id
 
   menu label: "Starting Tutorial Prompts"
+
+  index title: "Starting Tutorial Prompts" do
+    selectable_column
+    id_column
+    column :prompt
+    column :ai_model
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
