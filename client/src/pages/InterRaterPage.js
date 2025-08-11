796f6f06b6e9c8890efdac44024ba12afe083f58
Saruj Sattayanurak <saruj@Sarujs-MacBook-Pro.local>
2025-08-11 02:16:56 +0100

    Fix typo

--- client/src/pages/InterRaterPage.js
+++ client/src/pages/InterRaterPage.js
@@ -545,7 +545,7 @@ const InterRaterPage = () => {
                             onClick={() => {
                               if (item.first_conversation_fine_tune_data) {
                                 openTextModal(
-                                  'Fine-tuned Data',
+                                  'Fine-tune Data',
                                   pretty(item.first_conversation_fine_tune_data)
                                 );
                               }
